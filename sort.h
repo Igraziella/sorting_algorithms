528050abb320b4dbbd6a4a1b544753c090c1468a
igraziella <igraziella2@gmail.com>
2023-01-20 15:45:11 -0800

    header file

--- sort.h
+++ sort.h
@@ -32,11 +32,11 @@ typedef struct listint_s
 	const int n;
 	struct listint_s *prev;
 	struct listint_s *next;
-} listint_t
+} listint_t;
 
 
 /* print functions */
-void print_list(const listint_t *list)
+void print_list(const listint_t *list);
 void print_array(const int *array, size_t size);
 
 /* Algorithm prototypes */
