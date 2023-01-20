b210ccf2cb639fc7b5227b832fedcaa9bad3d1fd
igraziella <igraziella2@gmail.com>
2023-01-20 15:44:16 -0800

    sorting algos

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,4 +1,4 @@
-#include <sort.h>
+#include "sort.h"
 
 /**
  * bubble_sort - sorts an array of integers in ascending
@@ -33,6 +33,6 @@ void bubble_sort(int *array, size_t size)
 				print_array(array, size);
 			}
 		}
-		n = new;
+		n = new_n;
 	}
 }
